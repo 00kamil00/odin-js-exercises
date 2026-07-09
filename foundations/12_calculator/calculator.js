be480a246bf587be21cafae52c38841517a2018d
00kamil00 <kamilkalemba6@gmail.com>
2026-07-09 17:56:40 +0200

    Complete 12_calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+
+const sum = function(arr) {
+  let result = 0
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i]
+  }
+  return result
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  result = 1
+  for (let i = 0; i < arr.length; i++) {
+    result *= arr[i]
+  }
+  return result
 };
 
-const power = function() {
-	
+
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+
+const factorial = function(num) {
+  let result = 1
+  for (let i = num; i >= 1; i--) {
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
