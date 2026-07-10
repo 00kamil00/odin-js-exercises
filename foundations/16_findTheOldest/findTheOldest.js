b11a1cab693b942a58ec50aa69d2964b1dd48da9
00kamil00 <kamilkalemba6@gmail.com>
2026-07-11 00:32:43 +0200

    Complete 16_findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function getAge (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear()
+    }
+    return death - birth
+}
 
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+
+        if (oldestAge > currentAge) {
+            return oldest
+        } else {
+            return currentPerson
+        }
+    })
 };
 
 // Do not edit below this line
