cefb1b9908b53b718a94864bb2b7dad0fbe23301
00kamil00 <kamilkalemba6@gmail.com>
2026-07-10 22:03:27 +0200

    Complete 13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const smallletters = text.toLowerCase()
+    const cleartext = smallletters.replace(/[,\s\W_]+/g, "")
 
+    const arr = cleartext.split('')
+    const arr_reverse = arr.reverse()
+
+    const result = arr_reverse.join('')
+
+    if (cleartext === result) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
