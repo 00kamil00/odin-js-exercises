197d432564077d144675e1ea13f7a5a6a56c0413
00kamil00 <kamilkalemba6@gmail.com>
2026-07-11 00:11:57 +0200

    Complete 15_getTheTitles exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    const newarr = obj.map((booktitle) => {
+        return booktitle.title
+    })
+    return newarr
 };
 
 // Do not edit below this line
