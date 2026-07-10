36487999149660b84b8971f357533d27938f1a94
00kamil00 <kamilkalemba6@gmail.com>
2026-07-10 23:24:47 +0200

    Complete 14_fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+let num = number * 1
 
+if (num < 0) {
+    return "OOPS"
+} else if (num === 0 ) {
+    return 0
+}
+    let arr = [1, 1]
+    for (let i = 2; i < num; i++) {
+        let new_number = arr[i-1] + arr[i-2]
+        arr.push(new_number)
+    }
+    return arr.at(-1)
 };
 
 // Do not edit below this line
