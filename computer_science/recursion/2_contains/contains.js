6e90d3db2125e2ba4b5e6dfde216cc3b0bd7715f
00kamil00 <kamilkalemba6@gmail.com>
2026-09-13 20:16:32 +0200

    Complete 2_contains exercise

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,13 @@
-const contains = function() {
-  
+const contains = function(obj, searchValue) {
+    for (const val of Object.values(obj)) {
+        if (Object.is(val, searchValue)) return true
+
+        if (typeof val === 'object' && val !== null) {
+            if (contains(val, searchValue)) return true
+        }
+    }
+    return false
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
