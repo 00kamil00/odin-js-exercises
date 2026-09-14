6df870e9d285cb2a852db32090be7365115bf931
00kamil00 <kamilkalemba6@gmail.com>
2026-09-14 19:30:43 +0200

    Complete 4_permutations exercise

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,22 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+    if (arr.length <= 1) {
+        return [arr]
+    }
+
+    const result = []
+
+    for (let i = 0; i < arr.length; i++) {
+        const current = arr[i]
+
+        const remaining = arr.slice(0, i).concat(arr.slice(i + 1))
+        const remainingPerms = permutations(remaining)
+
+        for (const perm of remainingPerms) {
+            result.push([current, ...perm])
+        }
+    }
+    return result
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
