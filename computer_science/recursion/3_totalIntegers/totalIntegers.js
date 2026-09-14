a761433f59776d1abfd1d2073725c171d0b3ab22
00kamil00 <kamilkalemba6@gmail.com>
2026-09-14 18:53:48 +0200

    Complete 3_totalIntegers exercise

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,19 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+        return undefined
+    }
+
+    let count = 0
+    for (const element of Object.values(obj)) {
+        if (Number.isInteger(element)) {
+            count ++
+        }
+        if (typeof element === 'object' && element !== null) {
+            count += totalIntegers(element)
+        }
+    }
+    return count
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
