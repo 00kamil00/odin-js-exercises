9649cb2d1900284ab0c80fe785cd4040d1a88314
00kamil00 <kamilkalemba6@gmail.com>
2026-09-15 20:29:05 +0200

    Complete 5_pascal exercise

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,17 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+    if (n === 1) {
+        return [1]
+    }
+
+    const prevRow = pascal(n - 1)
+    const padded = [0, ...prevRow, 0]
+    const newRow = []
+
+    for (let i = 0; i < padded.length - 1; i++) {
+        newRow.push(padded[i] + padded[i + 1])
+    }
+    return newRow
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
